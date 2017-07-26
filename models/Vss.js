018e5d167ad43a5d8c6339beff03497acae3c3b0
tessellated-Dreams <tessellated.dreams@gmail.com>
2017-07-26 17:59:23 -0400

    refactored code to be slightly more modular

--- models/Vss.js
+++ models/Vss.js
@@ -8,7 +8,6 @@ const SQLTable = require('./SQLTable.js');
 const Inventory = require('./Inventory.js');
 
 /*
-
 ██╗   ██╗███████╗███████╗
 ██║   ██║██╔════╝██╔════╝
 ██║   ██║███████╗███████╗
@@ -28,96 +27,118 @@ TODO: Explain stuff better
 
 class Vss{
 
+    constructor(){
+        // Hold relevant db id's
+        this.id = {
+            store : 'd955f9f3-e9ae-475a-a944-237862b589b3', // VSS store
+            from : '5c637540-d460-4938-ac38-b6d283ea9a6d', // Man adjust facility
+            to : '83bcecf8-6881-4202-bb1c-051f77f27d90' // VSS facility
+        }
+    }// constructor
+
+    static callback(err, result){
+      console.log("CALLBACK CALLED");
+      if (err){
+        console.log(err);
+        return Page.renderNotFound(req, res)
+      }
+      console.log(result);
+      console.log("CALLBACK RESULT");
+      return result
+    }
+
+    static checkInventory(sku){
+        let vssFacility = '83bcecf8-6881-4202-bb1c-051f77f27d90';
+        Inventory.get(vssFacility, (err, result) =>{
+            console.log(result)
+            console.log(sku + ": " + result["inventory"][sku]);
+        });
+        return true
+    }
+
+    static handleGET(req, res){
+        // Check for required query string
+        if (Object.keys(req.query).length < 1)
+            return Page.renderNotFound(req, res)
+
+        // Grab a component representing the item to be sold
+        Component.get(req.query["sku"], (err, comp) => {
+            if (err) {
+              console.log(error);
+              return Page.renderNotFound(req, res)
+            }
+            // Check if we have inventory before rendering cartProps
+            if (Vss.checkInventory(req.query["sku"])) {
+                // Prep cart
+                let cartProps = {
+                  store: [{id: 'd955f9f3-e9ae-475a-a944-237862b589b3'}],
+                  items: [{props: comp.props}],
+                  ignoreWebCart: true,
+                  is_cart: true
+                }
+                // Render cart
+                return Cart.preprocessProps(cartProps, (err, options) => {
+                    return Page.render(req, res, Cart, options);
+                });
+            }
+            // No inventory!
+            return Page.renderNotFound(req, res)
+        });// Component.get()
+    }
+
+    static handlePOST(req, res){
+
+        //facility id's
+        var from_id = '5c637540-d460-4938-ac38-b6d283ea9a6d'; // Manual adjust
+        var to_id = '83bcecf8-6881-4202-bb1c-051f77f27d90'; // VSS
+        var store_id = 'd955f9f3-e9ae-475a-a944-237862b589b3'; // VSS store
+
+        if (typeof(req.body) == 'string') {
+            try {
+                req.body = JSON.parse(req.body);
+            } catch (err) {
+                console.log(err);
+                return res.json({error: "Malformed payload"});
+            }
+        }
+        //error handling of contents and shipment
+        // Create a new shipment and component
+        let received = Math.round(new Date().getTime()/1000);
+        let newComponent = new Component(req.body);
+        let contents = new Item(req.body);
+        let shipment = new Shipment({
+          from_id,
+          to_id,
+          store_id,
+          contents,
+          received
+        });
+        // Upsert to component and shipment tables
+        newComponent.upsert((err, result) => {
+            shipment.upsert((err, result)=> {
+              if (err)
+                return res.status(500).json({error: "Could not save shipment info"}).end();
+              res.json({ok: "ok", shipment: shipment}).end();
+            }); // shipment.upsert()
+        }); // newComponent.upsert()
+    }
+
     static handleHTTP(req, res, next){
-
-      var store_id = 'd955f9f3-e9ae-475a-a944-237862b589b3';  // VSS store
-      var from_id = '5c637540-d460-4938-ac38-b6d283ea9a6d';  //  Man adjust facility
-      var to_id = '83bcecf8-6881-4202-bb1c-051f77f27d90';  //  VSS facility
-
-      // Skip if nothing after '/'
-      // ex: www.bowanddrape.com/
+      // Skip if nothing after '/' ( ex: www.bowanddrape.com/ )
       if(req.path_tokens.length < 1)
         return next();
-      // Skip if we're going to any page other than '/'
-      // ex: www.bowanddrape.com/someOtherPage
+      // Skip if we're going to '/vss' ( ex: www.bowanddrape.com/someOtherPage )
       if(req.path_tokens[0].toLowerCase() !== 'vss')
         return next();
       // goto www.bowanddrape.com/vss/admin
       if(req.path_tokens.length > 1 && req.path_tokens[1].toLowerCase() === 'admin')
         return Page.render(req, res, VssAdmin, {});
-
       // Check for GET or POST to www.bowanddrape.com/vss/
-      if(req.path_tokens[0].toLowerCase() == 'vss')
-        // Handle GET request
-        if (req.method == 'GET'){
-            // Check for a query string
-            if (Object.keys(req.query).length < 1)
-                return Page.renderNotFound(req, res)
-            // Get cart stuff from table
-            Component.get(req.query["sku"], (err, result) => {
-                if (err){
-                    console.log("Error: " + err);
-                    return Page.renderNotFound(req, res)
-                }
-                // Check for a null result if no sku, props were found in the table
-                if (result){
-                  //  Error check, don't render a cart if someone already ordered it
-                  Inventory.get(to_id, (err, quantities => {
-                      if (err){
-                          console.log("Error: " + err);
-                      }
-                      // Check results for inventory quantity
-                      console.log("Inventory");
-                      console.log(quantities);
-                  }));
-
-                  //return Page.renderNotFound(req, res)
-
-                  //  Nobody ordered it yet, make a cart, YAY!
-                  return Page.render(req, res, Cart, {
-                    store: [ { id: store_id } ],
-                    items: [{props: result.props}],
-                    ignoreWebCart: true,
-                  });
-                }
-                // Return 404 if no component was found matching the sku
-                return Page.renderNotFound(req, res)
-            }); // Component.get()
-        }
-
-        //handle POST request
-        if (req.method == 'POST'){
-          if (typeof(req.body) == 'string') {
-              try {
-                  req.body = JSON.parse(req.body);
-              } catch (err) {
-                  console.log(err);
-                  return res.json({error: "Malformed payload"});
-              }
-          }
-          //error handling of contents and shipment
-
-          // Create a new shipment and component
-          let received = Math.round(new Date().getTime()/1000);
-          let newComponent = new Component(req.body);
-          let contents = new Item(req.body);
-          let shipment = new Shipment({
-            from_id,
-            to_id,
-            store_id,
-            contents,
-            received
-          });
-          // Upsert to component and shipment tables
-          newComponent.upsert((err, result) => {
-              shipment.upsert((err, result)=> {
-                if (err)
-                  return res.status(500).json({error: "Could not save shipment info"}).end();
-                res.json({ok: "ok", shipment: shipment}).end();
-              }); // shipment.upsert()
-          }); // newComponent.upsert()
-        }
+      if(req.path_tokens[0].toLowerCase() == 'vss'){
+        if (req.method == 'GET') Vss.handleGET(req, res);  // Handle GET request
+        if (req.method == 'POST') Vss.handlePOST(req, res);  //handle POST request
       }
-  }
+    }
+}
 
 module.exports=Vss;
