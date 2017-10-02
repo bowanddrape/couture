d6f4f9cfa753237defb4eb3b567e98c608eb8944
default <graphicsforge@gmail.com>
2017-10-02 11:18:02 -0400

    render 404 html responses
    removed some accidentially included files

---
+++ views/WarningNotice.jsx
@@ -0,0 +1,29 @@
+
+const React = require('react');
+const Items = require('./Items.jsx');
+const Signup = require('./Signup.jsx');
+
+/***
+Draw in the case of a 404
+***/
+
+class WarningNotice extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: this.props.message || [],
+    }
+  }
+
+  render() {
+
+    return (
+      <div className="warning404">
+        <h1>{this.state.message}</h1>
+      </div>
+    );
+  }
+}
+
+
+module.exports = WarningNotice;
