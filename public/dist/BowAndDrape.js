93256ddca28a4e66d43aae999a37bdfe79d60854
default <graphicsforge@gmail.com>
2017-11-22 11:19:40 -0500

    remove sale option from header menu

--- public/dist/BowAndDrape.js
+++ public/dist/BowAndDrape.js
@@ -23301,7 +23301,7 @@ module.exports={
         "spec": ">=6.0.0 <7.0.0",
         "type": "range"
       },
-      "/home/default/bowndrape/couture/node_modules/browserify-sign"
+      "/home/default/bowanddrape/couture/node_modules/browserify-sign"
     ]
   ],
   "_from": "elliptic@>=6.0.0 <7.0.0",
@@ -23336,7 +23336,7 @@ module.exports={
   "_shasum": "cac9af8762c85836187003c8dfe193e5e2eae5df",
   "_shrinkwrap": null,
   "_spec": "elliptic@^6.0.0",
-  "_where": "/home/default/bowndrape/couture/node_modules/browserify-sign",
+  "_where": "/home/default/bowanddrape/couture/node_modules/browserify-sign",
   "author": {
     "name": "Fedor Indutny",
     "email": "fedor@indutny.com"
@@ -65978,7 +65978,7 @@ var Item = function (_React$Component) {
             remove: ["new", "needs_picking"]
           },
           "needs_pressing": {
-            add: [["needs_qaing"]],
+            add: [["needs_qaing"], ["needs_picking"]],
             remove: ["needs_pressing"]
           },
           "needs_qaing": {
@@ -66890,15 +66890,6 @@ var LayoutHeader = function (_React$Component) {
           'button',
           { className: 'primary' },
           'Gift Guide'
-        )
-      ));
-      menu_items.push(React.createElement(
-        'a',
-        { key: menu_items.length, href: '/rtw_sale' },
-        React.createElement(
-          'button',
-          { className: 'primary' },
-          'SALE'
         )
       ));
       // links to admin pages
