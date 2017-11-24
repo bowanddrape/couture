e0fc6564cb15b26340f345f412b8ccec66128889
tessellated-Dreams <tessellated.dreams@gmail.com>
2017-11-24 12:14:44 -0500

    Added table generation for metrics pulled from the db

--- views/MetricsDash.jsx
+++ views/MetricsDash.jsx
@@ -35,21 +35,66 @@ class MetricsDash extends React.Component {
           return err;
         }
         this.setState((prevState)=>{
-            return ({hasMetrics: true, metrics: result.metrics["rows"]});
+          return ({
+            hasMetrics: true,
+            metricsData: result,
+          });
         });
       });
     }
 
     renderMetrics() {
+      // fill in table data!
       if (this.state.hasMetrics){
         let queries = [];
-        this.state.metrics.forEach((query, index)=>{
-          let type = Object.keys(query)[0];
+        this.state.metricsData.metrics.forEach((query, index)=>{
+          let tableTitle = query["format"]["title"];
+          let columnNames = query["format"]["columnNames"];
+          let type = query["format"]["type"];
+          let numCols = query["format"]["columns"];
+          // Construct our header of column names
+          let header = [];
+          columnNames.forEach((name, index)=>{
+            header.push(<th key={index}>{name}</th>);
+          });
+          // Fill in our data
+          let columnData = [];
+          let data = query["data"][0][type];
+          // switch based on column numbers
+          switch(numCols){
+            case 1:
+              columnData.push(
+                <tr key={columnData.length}>
+                  <td>{data}</td>
+                </tr>
+              );
+              break;
+
+            case 2:
+              for (let key in data){
+                if (data.hasOwnProperty(key)){
+                  columnData.push(
+                    <tr key={columnData.length}>
+                      <td>{key}</td>
+                      <td>{data[key]}</td>
+                    </tr>
+                  );
+                }
+              }
+              break;
+            } // switch
+
           queries.push(
-            <tr key={queries.length}>
-              <td>{type}</td>
-              <td>{query[type]}</td>
-            </tr>
+            <table key={queries.length}>
+              <thead>
+                <tr>
+                  {header}
+                </tr>
+              </thead>
+              <tbody>
+                 {columnData}
+              </tbody>
+            </table>
           );
         });
         return queries;
@@ -59,6 +104,7 @@ class MetricsDash extends React.Component {
 
     render() {
       let metrics = this.renderMetrics();
+      //let metrics = null;
       return(
         <div className="metrics_dash">
           <section>Search Metrics</section>
@@ -84,11 +130,7 @@ class MetricsDash extends React.Component {
             </row>
             <button onClick={this.handleSearch.bind(this)}>Search</button>
             <div className="display_metrics">
-              <table>
-                <tbody>
-                  {metrics}
-                </tbody>
-              </table>
+              {metrics}
             </div>
         </div>
       );
