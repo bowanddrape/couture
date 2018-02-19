44682db6926306ea637c1c8e9df9aa822e6bef00
default <graphicsforge@gmail.com>
2018-02-19 16:13:48 -0500

    add fullstory.com user tracking

--- views/LayoutMain.jsx
+++ views/LayoutMain.jsx
@@ -93,6 +93,25 @@ class LayoutMain extends React.Component {
         `}} >
         </script>
 
+        {/* Fullstory tracking script */}
+        <script dangerouslySetInnerHTML={{__html:`
+          window['_fs_debug'] = false;
+          window['_fs_host'] = 'fullstory.com';
+          window['_fs_org'] = 'AKQJP';
+          window['_fs_namespace'] = 'FS';
+          (function(m,n,e,t,l,o,g,y){
+            if (e in m) {if(m.console && m.console.log) { m.console.log('FullStory namespace conflict. Please set window["_fs_namespace"].');} return;}
+            g=m[e]=function(a,b){g.q?g.q.push([a,b]):g._api(a,b);};g.q=[];
+            o=n.createElement(t);o.async=1;o.src='https://'+_fs_host+'/s/fs.js';
+            y=n.getElementsByTagName(t)[0];y.parentNode.insertBefore(o,y);
+            g.identify=function(i,v){g(l,{uid:i});if(v)g(l,v)};g.setUserVars=function(v){g(l,v)};
+            y="rec";g.shutdown=function(i,v){g(y,!1)};g.restart=function(i,v){g(y,!0)};
+            g.identifyAccount=function(i,v){o='account';v=v||{};v.acctId=i;g(o,v)};
+            g.clearUserCookie=function(){};
+          })(window,document,window['_fs_namespace'],'script','user');
+          `}} >
+        </script>
+
         {/* Zopim Live Chat Script */}
         <script dangerouslySetInnerHTML={{__html:`// &lt;![CDATA[
           window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s= d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set. _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8'); $.src='//cdn.zopim.com/?Q4N5gKntLCE7FT6MI3AtGRDck6kwMqzP';z.t=+new Date;$. type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
